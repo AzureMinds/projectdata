eb6929a3c7a1e4102987a0e709353e90b8ae0d1c
AzureMind <46114014+patstew123@users.noreply.github.com>
2024-09-23 14:02:25 +0100

    new changes added

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Building a personal website using Jekyll",
+        "excerpt":"So, after much procrastination, I have finally put together a personal website (of sorts) to keep better track of blog posts, personal projects and general ramblings. In the interest of clarity and for those of you interested, here is how I put this together and an explanation of what are...","categories": ["blog","personal"],
+        "tags": ["jekyll"],
+        "url": "/blog/personal/2024/09/04/building_a_blog.html",
+        "teaser": null
+      }]
--- index.md
+++ index.md
@@ -4,17 +4,8 @@ title: "Welcome to Azure Minds"
 permalink: /
 ---
 
-# Welcome to Azure Minds
-
 Writing about all things machine learning and data engineering.
 
 ## About Me
 
 Hello! I'm Patrick Stewart, a data engineer and machine learning enthusiast. I love exploring the intersections of data science, machine learning, and natural language processing.
-
-## Latest Posts
-
-{% for post in site.posts %}
-  <h2><a href="{{ post.url }}">{{ post.title }}</a></h2>
-  <p>{{ post.excerpt }}</p>
-{% endfor %}
